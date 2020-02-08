76416e22ff4ade743b55d1b890bfdb909f65aca4
Espertinhos <murillo_1230@hotmail.com>
2020-02-07 22:27:59 -0300

    Aula01 De Java Murillo

---
+++ src/Nadador.java
@@ -0,0 +1,24 @@
+
+public class Nadador {
+
+	public static void main(String[] args) {
+		
+		int idade = 11;
+		
+		if (idade <= 7) {
+			System.out.println("Categoria Infantil");
+		} else if(idade >= 8 && idade <= 10) {
+			System.out.println("Categoria Juvenil");
+		} else if (idade >= 11 && idade <= 15) {
+			System.out.println("Categoria Adolescente");
+		} else if (idade >= 16 && idade <= 30) {
+			System.out.println("Categoria Adulto");
+		} else {
+			System.out.println("Categoria Senior");
+		}
+	}
+	
+	
+	
+			
+}
